25aa06b2d21cd13f734b92a14d51c7b3a5fb4f80
Furkan Gökoluk <furkangokoluk@outlook.com>
2024-01-23 22:16:28 +0300

    Veri tabanı gizliliği..

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php
-        $connection = mysqli_connect("localhost","root","");
+        $connection = mysqli_connect("Sunucu_adi","kullanici_adi","sifre");
         if (!$connection)
         {
             die("Veritabanı Bağlantısı Hatası!!!".mysqli_connect_error());
